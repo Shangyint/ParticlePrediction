bf51a72d6ebd25076b4a5798827712ff23beb200
Shangyin Tan <tan279@purdue.edu>
2019-10-16 02:51:41 -0400

    3d plot

--- visualizer/main.py
+++ visualizer/main.py
@@ -3,8 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-os.chdir('../..')
 
+current_data_dir = 'test_0'
+os.chdir('../data/' + current_data_dir)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+df = pd.read_csv('sphere1.csv')
+sphere_info = df.values
+ax.plot3D(sphere_info[:, 0], sphere_info[:, 1], sphere_info[:, 2], 'gray')
+df = pd.read_csv('sphere2.csv')
+sphere_info = df.values
+ax.plot3D(sphere_info[:, 0], sphere_info[:, 1], sphere_info[:, 2], 'green')
+plt.show()
+
+# back to original directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
